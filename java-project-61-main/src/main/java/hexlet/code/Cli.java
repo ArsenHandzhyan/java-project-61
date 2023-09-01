b06927dfd7d7400a47a4f8e4f1571fe335436d2c
ArsenHandzhyan <arsenhan@internet.ru>
2023-09-01 14:00:31 +0300

    add checkstyle file

--- java-project-61-main/src/main/java/hexlet/code/Cli.java
+++ java-project-61-main/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void greetUser(){
+    public static void greetUser() {
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
         Scanner sc = new Scanner(System.in);
