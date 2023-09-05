d09f0d69012120af6663105c725522a3a7fb846c
ArsenHandzhyan <arsenhan@internet.ru>
2023-09-05 03:18:07 +0300

    add project files

---
+++ java-project-61-main/app/src/main/java/hexlet/code/games/Even.java
@@ -0,0 +1,49 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Even {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void gameStart1() {
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        game(0);
+    }
+
+    public static int randomGen() {
+        Random random = new Random();
+        return random.nextInt(101);
+    }
+
+    public static String evenNumber(int evenNum) {
+        if (evenNum % 2 == 0) {
+            return "yes";
+        } else {
+            return "no";
+        }
+    }
+
+    public static void game(int count) {
+        int evenNum = randomGen();
+        System.out.println("Question: " + evenNum);
+        String input = sc.nextLine();
+        String even = evenNumber(evenNum);
+        if (count < 2) {
+            if (input.equals(even)) {
+                System.out.println("Correct!");
+                game(++count);
+            } else {
+                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
+                Engine.again();
+                Engine.gameEngine();
+            }
+        } else {
+            Engine.congratulations();
+            Engine.gameEngine();
+        }
+    }
+}
+
