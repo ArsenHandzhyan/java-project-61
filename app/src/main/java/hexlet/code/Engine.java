e3440493b7c1adbcba6cd970e6872a0ed431fc45
arsen <arsenhan@mail.ru>
2023-10-11 21:07:21 +0300

    fix Engine class

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,26 +4,23 @@ import static hexlet.code.Utils.getString;
 
 public class Engine {
     private static String playerName;
-    private static final int RESULT_INDEX = 0;
-    private static final int QUESTIONS_INDEX = 1;
-    private static final int COUNT_INDEX = 2;
 
-    public static void run(String description, String[][] roundsData, int count) {
-        String countS = roundsData[count][COUNT_INDEX];
-        count = Integer.parseInt(countS);
-        String result = roundsData[count][RESULT_INDEX];
-        String questions = roundsData[count][QUESTIONS_INDEX];
+    public static void run(String description, String[][] roundsData, int countNewGame) {
+        String result = roundsData[countNewGame][0];
+        String questions = roundsData[countNewGame][1];
+        String countS = roundsData[countNewGame][2];
+        int count = Integer.parseInt(countS);
         if (count == 0) {
             System.out.println("\nWelcome to the Brain Games!");
             System.out.print("May I have your name? ");
             String name = getString();
-            System.out.print("\nHello, " + name + "!\n");
+            System.out.print("Hello, " + name + "!\n");
             playerName = name;
             System.out.println(description);
         }
         System.out.println("Question: " + questions);
-        System.out.print("Your answer: ");
         String input = getString();
+        System.out.println("Your answer: " + input);
         if (count <= 2) {
             if (input.equals(result)) {
                 System.out.println("Correct!");
@@ -41,5 +38,4 @@ public class Engine {
         }
     }
 }
-
 
